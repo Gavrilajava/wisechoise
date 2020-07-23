ad2b4ae924e9cb32721a97b0c2a9ba5b76c2b6a8
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-07-23 13:32:04 -0500

    ignore modules3

--- server/app.js
+++ server/app.js
@@ -1,3 +1,20 @@
 const express = require('express')
-
 const app = express()
+
+const mongoose = require('mongoose')
+
+
+
+app.listen(3000)
+
+// mongodb+srv://wcUser:<password>@wisechoise.ijjrr.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://wcUser:<password>@wisechoise.ijjrr.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
